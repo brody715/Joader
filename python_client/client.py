6a2f1eff93b4053706d41d0454a23cb40c3173a2
xiejiann <1258212370@qq.com>
2021-09-21 14:59:29 +0800

    task_manager

--- python_client/client.py
+++ python_client/client.py
@@ -1,9 +1,9 @@
-import grpc
-import task_pb2
-import task_pb2_grpc
+# import grpc
+# import task_pb2
+# import task_pb2_grpc
 
-channel = grpc.insecure_channel('0.0.0.0:5688')
-client = task_pb2_grpc.TaskStub(channel)
-request = task_pb2.CreateTaskRequest(keys = ["1", "2", "3"], weights = [1,2,3])
-resp = client.Create(request)
-print(resp)
+# channel = grpc.insecure_channel('0.0.0.0:5688')
+# client = task_pb2_grpc.TaskStub(channel)
+# request = task_pb2.CreateTaskRequest(keys = ["1", "2", "3"], weights = [1,2,3])
+# resp = client.Create(request)
+# print(resp)
