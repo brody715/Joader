014737b2205230e27af28988a170fabc9f8e8f76
xiejiann <jianxie0@gmail.com>
2021-12-13 05:03:49 +0000

    fix

--- client/test_dataset.py
+++ client/test_dataset.py
@@ -2,10 +2,10 @@ from dataset.dataset import Dataset, DatasetType
 import grpc
 
 
-channel = grpc.insecure_channel('127.0.0.1:4321')
-name = "ImageNet"
+channel = grpc.insecure_channel('210.28.134.91:4321')
+name = "DUMMY"
 if __name__ == "__main__":
-    ds = Dataset(name=name, ty=DatasetType.DUMMY)
+    ds = Dataset(name=name, ty=DatasetType.DUMMY, location="")
     for i in range(0, 100):
         ds.add_item([str(i)])
     ds.create(channel)
