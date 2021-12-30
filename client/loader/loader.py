6e838af8c17854d0e4882062a8df8a5e0adae011
xiejiann <jianxie0@gmail.com>
2021-12-30 02:20:13 +0000

    fix

--- client/loader/loader.py
+++ client/loader/loader.py
@@ -1,3 +1,4 @@
+from os import read
 import grpc
 import sys
 sys.path.append("./proto")
@@ -6,15 +7,14 @@ from loader.shm import SharedMemory
 import proto.dataloader_pb2_grpc as dataloader_pb2_grpc
 import proto.dataloader_pb2 as dataloader_pb2
 import socket
-import time
-import logging
-
-
+import multiprocessing
+import threading
 
 class Loader(object):
-    def __init__(self, ip, length: int, loader_id: int, shm_path: str, name: str, dataset_name: str, server_ip: str, nums: int, bs: int):
+    def __init__(self, ip, length: int, loader_id: int, shm_path: str, name: str, dataset_name: str, server_ip: str, nums: int, bs: int, queue: multiprocessing.Queue, client_thread):
         self.length = length
         self.client = None
+        self.client_thread = client_thread
         self.loader_id = loader_id
         self.shm_path = shm_path
         self.shm = SharedMemory(self.shm_path)
@@ -24,8 +24,6 @@ class Loader(object):
         self.channel = None
         self.server_ip = server_ip
         self.bs = bs
-        self.address = []
-        self.read_off = []
 
         self.HEAD_SIZE = 20
         self.READ_OFF = 12
@@ -36,21 +34,41 @@ class Loader(object):
         self.OFF_LEN = 8
         self.ip = ip
         self.nums = nums
-        logging.basicConfig(filename='/home/xiej/ATC/log/py_loader.log', level=logging.DEBUG)
         #profile
         self.read_time = 0
         self.rpc_time = 0
+        self.queue = queue
+    
     @staticmethod
-    def new(dataset_name: str, name: str, ip: str, nums: int = 1, batch_size: int = 1):
-        # nums indicate the number of distributed tasks
+    def run_client(dataset_name: str, name: str, ip: str, nums: int, batch_size, q):
         channel = grpc.insecure_channel(ip)
         client = dataloader_pb2_grpc.DataLoaderSvcStub(channel)
         request = dataloader_pb2.CreateDataloaderRequest(
             dataset_name=dataset_name, name=name, nums=nums)
         resp = client.CreateDataloader(request)
-        # close to enable multi process grpc
-        channel.close()
-        return Loader(ip, resp.length, resp.loader_id, resp.shm_path, name, dataset_name, ip, nums, batch_size)
+        loader_id = resp.loader_id
+        length = resp.length
+        q.put(resp.length)
+        q.put(resp.loader_id)
+        q.put(resp.shm_path)
+        while length > 0:
+            request = dataloader_pb2.NextRequest(loader_id=loader_id, batch_size=batch_size)
+            resp = client.Next(request)
+            read_off_list = resp.read_off
+            address_list = resp.address
+            for (read_off, address) in zip(read_off_list,address_list):
+                q.put((read_off, address))
+            length -= len(read_off_list)
+    @staticmethod
+    def new(dataset_name: str, name: str, ip: str, nums: int = 1, batch_size: int = -1):
+        q = multiprocessing.Queue()
+        t = threading.Thread(target=Loader.run_client, args=(dataset_name, name, ip, nums, batch_size, q,))
+        t.start()
+        # nums indicate the number of distributed tasks
+        length = q.get()
+        loader_id = q.get()
+        shm_path = q.get()
+        return Loader(ip, length, loader_id, shm_path, name, dataset_name, ip, nums, batch_size, q, t)
 
     def get_host_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -76,32 +94,18 @@ class Loader(object):
         return int(address/self.HEAD_SIZE)
     
     def read_one(self):
-        now = time.time()
-        addr = self.address.pop()*self.HEAD_SIZE
-        read_off = self.read_off.pop()
-        data = self.read_data(addr, read_off)
-        self.read_time += time.time() - now
+        self.read_off, self.addr = self.queue.get()
+        self.addr = self.addr*self.HEAD_SIZE
+        data = self.read_data(self.addr, self.read_off)
         return data
 
     def next(self):
-        logging.debug("%s start", self.name)
         assert self.length > 0
-        if self.channel is None:
-            self.channel = grpc.insecure_channel(self.server_ip)
-            self.client = dataloader_pb2_grpc.DataLoaderSvcStub(self.channel)
         self.length -= 1
-        if len(self.read_off) == 0:
-            now = time.time()
-            request = dataloader_pb2.NextRequest(loader_id=self.loader_id, batch_size=self.bs)
-            resp = self.client.Next(request)
-            self.read_off = resp.read_off
-            self.address = resp.address
-            self.rpc_time = time.time() - now
-            logging.debug("%s read addr %s, %s", self.name, self.address, self.read_off)
-            # print(len(self.read_off), self.bs, self.rpc_time, self.read_time)
-            self.rpc_time = 0
-            self.read_time = 0
         return self.read_one()
+
+    def readed(self):
+        self.buf[self.addr+self.READ_OFF + self.read_off] = 0
 
     def delete(self):
         if self.channel is None:
