7c0bf3d075e6dcdcdb7155210ec7a0e3e92393e6
xiejiann <jianxie0@gmail.com>
2022-05-17 11:32:02 +0000

    fix config

--- server/src/joader/joader.rs
+++ server/src/joader/joader.rs
@@ -107,7 +107,8 @@ impl Joader {
         }
         let sample_res = {
             let mut sampler_tree_lock = self.sampler_tree.lock().await;
-            sampler_tree_lock.sample_with_buffer(&mask)
+            // sampler_tree_lock.sample_with_buffer(&mask)
+            sampler_tree_lock.sample(&mask)
         };
         log::debug!(
             "sampling result (data_set, job_set){:?} with mask {:?}",
--- server/src/main.rs
+++ server/src/main.rs
@@ -31,7 +31,7 @@ async fn start_server(ip: &str, port: &str) -> Result<(), Box<dyn std::error::Er
     log::info!("start server");
     let id_gen = IdGenerator::new();
     let dataset_id_table = Arc::new(Mutex::new(HashMap::new()));
-    let cache = Arc::new(Mutex::new(Cache::with_capacity(224*224*3*32*(1024))));
+    let cache = Arc::new(Mutex::new(Cache::with_capacity(224*224*3*32*(128))));
     let joader_table = Arc::new(Mutex::new(JoaderTable::new(cache)));
     let ip_port = ip.to_string() + ":" + port;
     let addr: SocketAddr = ip_port.parse()?;
@@ -58,7 +58,7 @@ async fn start_server(ip: &str, port: &str) -> Result<(), Box<dyn std::error::Er
     Ok(())
 }
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 8)]
+#[tokio::main(flavor = "multi_thread", worker_threads = 48)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log4rs_config = "/home/xiej/ATC/DLCache/server/log4rs.yaml";
     let ip = "0.0.0.0";
